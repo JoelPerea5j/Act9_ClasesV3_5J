548b0ba1288833dcea05894496cd843774291c3e
JoelPerea5j <a.22308051281272@cbtis128.edu.mx>
2024-09-20 09:26:36 -0600

    Add files via upload

---
+++ OperadoresA.py
@@ -0,0 +1,64 @@
+print("Act 9 Clases V3")
+print("Joel Perea Mat: 22308051281272de")
+# Zona de class
+
+class Operadores1272:
+    # Zona de Funciones
+    def suma1272(self,J,P):
+        s1272=J+P
+        print(f"La suma de {J} + {P} = {s1272}")
+
+    def resta1272(self,J,P):
+        r1272=J-P
+        print(f"La Resta de {J} - {P} = {r1272}")
+
+    def Multi1272(self,J,P):
+        M1272=J*P
+        print(f"La Multiplicacion de {J} * {P} = {M1272}")
+
+    def Divi1272(self,J,P):
+        D1272=J/P
+        print(f"La Divicion de {J} / {P} = {D1272}")
+     
+    def Model1272(self,J,P):
+        Mol1272=J%P
+        print(f"El Modelo de {J} % {P} = {Mol1272}")
+
+    def Expo1272(self,J,P):
+        E1272=J**P
+        print(f"El Exponente de {J} ** {P} = {E1272}")
+
+    def Coci1272(self,J,P):
+        C1272=J//P
+        print(f"El Cociente de {J} // {P} = {C1272}")
+
+# Zona de Creacion del Objeto
+OperaJoel=Operadores1272()
+OperadJoel=Operadores1272()
+OperadoJoel=Operadores1272()
+OperadorJoel=Operadores1272()
+OperadoreJoel=Operadores1272()
+OperadoresJoel=Operadores1272()
+OperadoressJoel=Operadores1272()
+
+# Zona De Uso De Objetos 
+print(" Funcion Suma")
+OperaJoel.suma1272(5,5)
+
+print(" Funcion Resta")
+OperadJoel.resta1272(10,5)
+
+print(" Funcion Multiplicacion")
+OperadoJoel.Multi1272(5,5)
+
+print(" Funcion Divicion")
+OperadorJoel.Divi1272(5,5)
+
+print(" Funcion Model")
+OperadoreJoel.Model1272(5,5)
+
+print(" Funcion Exponente")
+OperadoresJoel.Expo1272(3,3)
+
+print(" Funcion Cociente")
+OperadoressJoel.Coci1272(3,3)
